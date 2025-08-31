68324b2c60be9abb57ad37db3b6a50c032e8cbfb
SaiPh3r <saianand750@gmail.com>
2025-08-31 17:12:09 +0530

    Create Embedding_demo.py

---
+++ 3.EmbeddedModels/Embedding_demo.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+load_dotenv()
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+result = embeddings.embed_query("Delhi is capital of India")
+print(str(result))
